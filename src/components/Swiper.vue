7297814bd919520e92174f48891feb336ec446ff
khakimdon <almamedovkh@mail.ru>
2023-08-22 22:28:09 +0500

    commit

---
+++ src/components/Swiper.vue
@@ -0,0 +1,70 @@
+<template>
+    <swiper :pagination="{
+        type: 'fraction',
+    }" :navigation="true" :modules="modules" class="mySwiper">
+        <swiper-slide>
+            <div class="content">
+                <h2>Lorem ipsum dolor si Lorem, <br> ipsum dolor.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem distinctio amet culpa voluptate, fuga iure? Tempore sunt animi sed saepe et, dicta magni beatae veniam corporis sit ipsam eos soluta eveniet consectetur inventore? Minima ipsa quidem, assumenda veniam asperiores numquam.
+                </p>
+                <button>click this</button>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="content">
+                <h2>Lorem ipsum dolor sit Lorem, <br> ipsum dolor</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem distinctio amet culpa voluptate, fuga iure? Tempore sunt animi sed saepe et, dicta magni beatae veniam corporis sit ipsam eos soluta eveniet consectetur inventore? Minima ipsa quidem, assumenda veniam asperiores numquam.
+                </p>
+                <button>click this</button>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="content">
+                <h2>Lorem ipsum dolor sit Lorem,<br>  ipsum dolor</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem distinctio amet culpa voluptate, fuga iure? Tempore sunt animi sed saepe et, dicta magni beatae veniam corporis sit ipsam eos soluta eveniet consectetur inventore? Minima ipsa quidem, assumenda veniam asperiores numquam.
+                </p>
+                <button>click this</button>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="content">
+                <h2>Lorem ipsum dolor sit Lorem, <br> ipsum dolor</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem distinctio amet culpa voluptate, fuga iure? Tempore sunt animi sed saepe et, dicta magni beatae veniam corporis sit ipsam eos soluta eveniet consectetur inventore? Minima ipsa quidem, assumenda veniam asperiores numquam.
+                </p>
+                <button>click this</button>
+            </div>
+        </swiper-slide>
+    </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import '../assets/swiper.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        return {
+            modules: [Pagination, Navigation],
+        };
+    },
+};
+</script>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,26 @@
 <script>
-export default{
-  methods:{
-    sending(){
-      let btn = document.querySelector(".send")
-      let sendedtext = document.querySelector(".sended-text")
-      btn.style.animationPlayState = 'running'
-      sendedtext.classList.add("sended-text-activated")
-    }
+import Swiper from "../components/Swiper.vue"
+export default {
+  methods: {
+    sending() {
+      let btn = document.querySelector(".send");
+      let sendedtext = document.querySelector(".sended-text");
+      btn.style.animationPlayState = "running";
+      sendedtext.classList.add("sended-text-activated");
+    },
+  },
+  components:{
+    Swiper
   }
-}
+};
 </script>
 
 <template>
   <main>
     <section class="one-screen">
+      <figure class="mobile-logo">
+        <img src="../assets/logo-5000pix.png" alt="" />
+      </figure>
       <div class="wrapper">
         <div class="left">
           <h1>
@@ -24,12 +31,13 @@ export default{
             kanalining rasmiy saytiga xush kelibsiz.
           </h1>
           <form @submit.prevent="sending()" action="#">
-            <input placeholder="Ismingiz" class="txt" type="text" />
-            <input placeholder="Telefon raqam" class="txt" type="text" />
+            <input required placeholder="Ismingiz" class="txt" type="text" />
+            <input required placeholder="Telefon raqam" class="txt" type="text" />
             <div class="send-wp">
               <input class="send" value="Ariza qoldirish" type="submit" />
-              <span class="sended-text">Arizangiz qabul qilindi. <br> Siz bilan tez orada ulanamiz.</span>
-            </div> 
+              <span class="sended-text">Arizangiz qabul qilindi. <br />
+                Siz bilan tez orada ulanamiz.</span>
+            </div>
           </form>
         </div>
         <div class="right">
@@ -38,21 +46,131 @@ export default{
           </div>
           <div class="blocks">
             <div class="items">
-              <span>Onlayn <br> Konsultatsiya</span>
+              <span>Onlayn <br />
+                Konsultatsiya</span>
             </div>
             <div class="items">
-              <span>O'qishga <br> kirish</span>
+              <span>O'qishga <br />
+                kirish</span>
             </div>
             <div class="items">
               <span>Grant 2024</span>
             </div>
             <div class="items">
-              <span>O'qishni <br> ko'chirish</span>
+              <span>O'qishni <br />
+                ko'chirish</span>
             </div>
           </div>
         </div>
       </div>
     </section>
-    <section class="second-screen"></section>
+    <section class="second-screen">
+      <div class="wrapper">
+        <div class="left">
+          <h1>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+            </svg>
+            Nega aynan biz ?
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aut
+            nemo quidem? Repellendus quos nemo, laudantium sint aut tempore
+            placeat impedit commodi, distinctio error laboriosam odio cupiditate
+            vel repudiandae corporis odit!
+            <span>
+              Commodi animi blanditiis quibusdam amet nihil unde officiis libero
+              culpa eos </span>consequuntur rem ullam excepturi id dolor quidem iusto fugiat quos
+            tempore, illum dolorem officia dolore recusandae sint. Rerum magni
+            quaerat necessitatibus illum aut dignissimos soluta obcaecati
+            explicabo molestias animi! Voluptatem adipisci sed, dicta aperiam,
+            deleniti animi cum similique exercitationem sequi voluptate commodi
+            neque error necessitatibus vitae. Cum tempore voluptatum expedita?
+            <br />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+            </svg>
+          </p>
+          <div></div>
+        </div>
+        <div class="right">
+          <div class="block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+            </svg>
+            FAST
+          </div>
+          <div class="block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5" />
+            </svg>
+            Correct
+
+          </div>
+          <div class="block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+</svg>
+document
+          </div>
+          <div class="block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+</svg>
+economy
+          </div>
+          <div class="block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+</svg>
+materials
+
+          </div>
+          <div class="block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
+</svg>
+feedbacks
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="slider-wrapper">
+      <Swiper/>
+    </section>
+
+    <section class="questions">
+
+      <h2>Ko`p  so`raladigan savollar :</h2>
+      <div class="wrapper">
+        <div class="left">
+          <img src="../assets/logo-5000pix.png" alt="">
+          <h2>Telegram kanalimizga a`zo bo`ling va barcha yangilioklardan xabardor bo`ling !</h2>
+          <button>Kanalga a'zo b`lish</button>
+        </div>
+        <div class="right">asds</div>
+      </div>
+
+    </section>
+
   </main>
 </template>
+<style>
+
+</style>
